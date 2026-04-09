cf40cb01a5bd0a147060f329dbe2dd0c2a8a7845
Daniel Lena <daniel.lena@koahealth.com>
2026-04-09 14:57:19 +0200

    Remove unreferenced isLoading itemlist

--- lib/item_list/data/item.dart
+++ lib/item_list/data/item.dart
@@ -8,8 +8,4 @@ class Item {
   final String name;
 
   Item({required this.color, required this.name, required this.price});
-
-  Item.loading() : this(color: Colors.grey, name: '...', price: 0);
-
-  bool get isLoading => name == '...';
 }
